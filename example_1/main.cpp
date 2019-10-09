91857d7f9d5b7e2f8e1e5a27b1e45901869dc1b7
Mikhail Kasparyan <m.kasparyan@innopolis.ru>
2019-10-09 23:52:03 +0300

    fix

--- example_1/main.cpp
+++ example_1/main.cpp
@@ -1,8 +1,23 @@
 #include <iostream>
 #include <flatbuffers/flatbuffers.h>
+#include "person_generated.h"
 using namespace std;
 
 int main(){
 	flatbuffers::FlatBufferBuilder builder;
+	auto name = builder.CreateString("Mikhail");
+	short age = 99;
+	auto surname = builder.CreateString("Kasparyan");
+	auto person = InnoPlusPlus::example_1::CreatePerson(builder, name, age, surname);
+	builder.Finish(person);
+	uint8_t* buf = builder.GetBufferPointer();
+	int sz = builder.GetSize();
+	
+	for (int i = 0; i < sz; ++i) {
+		cout << static_cast<int>(buf[i]) << " ";
+	}
+
+	auto parsed_person = InnoPlusPlus::example_1::GetPerson(buf);
+	cout << endl << parsed_person->name()->str();
     return 0;
 }
